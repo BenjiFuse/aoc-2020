3c0f95a5e7a3572e9f457be87daffe7ff755cadf
benjifuse <benjaminfuson4@gmail.com>
2020-12-02 22:02:17 -0500

    Day 2 complete

---
+++ Day2/Part1.py
@@ -0,0 +1,19 @@
+import re
+from itertools import accumulate
+
+pattern = r'(\d+)-(\d+) (.): (.+)'
+regex = re.compile(pattern)
+
+def is_valid(s):
+    m = regex.fullmatch(s)
+    min = int(m.group(1))
+    max = int(m.group(2))
+    char = m.group(3)
+    password = m.group(4)
+    count = password.count(char)
+    return min <= count <= max
+
+with open("input.txt") as f:
+    passwords = [l.strip() for l in f.readlines()]
+    valid_count = sum(map(is_valid, passwords))
+    print("Number of valid passwords: {0}".format(valid_count))
---
+++ Day2/Part2.py
@@ -0,0 +1,19 @@
+import re
+from itertools import accumulate
+
+pattern = r'(\d+)-(\d+) (.): (.+)'
+regex = re.compile(pattern)
+
+def is_valid(s):
+    m = regex.fullmatch(s)
+    min = int(m.group(1))
+    max = int(m.group(2))
+    char = m.group(3)
+    password = m.group(4)
+    print("{0} ==> {1}".format(s, (password[min-1] == char) ^ (password[max-1] == char)))
+    return (password[min-1] == char) ^ (password[max-1] == char)
+
+with open("input.txt") as f:
+    passwords = [l.strip() for l in f.readlines()]
+    valid_count = sum(map(is_valid, passwords))
+    print("Number of valid passwords: {0}".format(valid_count))
