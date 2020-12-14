2f47657955bde9aa8c8ec3a6f9950cb1c8d54117
benjifuse <benjaminfuson4@gmail.com>
2020-12-14 11:20:08 -0500

    Day 13 complete

--- Day13/Part2.py
+++ Day13/Part2.py
@@ -1,25 +1,46 @@
-from math import gcd
-from functools import reduce
+def extended_euclidean(a, b):
+    """Implementation of the extended euclidean algorithm for finding gcd
+    and x,y to satisfy a*x + b*y == gcd"""
+    if a == 0:
+        return b, 0, 1
+    
+    gcd, x1, y1 = extended_euclidean(b%a, a)
 
-def lcm(l):
-    return reduce(lambda a,b: a*b // gcd(a,b), l)
+    x = y1 - (b//a) * x1
+    y = x1
 
-def is_proper_offset(times):
-    return
+    return gcd, x, y
 
-def find_index_where_offset(a, b, offset):
+def mod_inverse(a, m):
+    g, x, y = extended_euclidean(a, m)
+    return x % m
+
+def crt(m, x):
+    """Implementation of Chinese Remainder Theorem for combining modular equations"""
+    while True:
+        an = mod_inverse(m[1], m[0]) * x[0] * m[1] + \
+                mod_inverse(m[0], m[1]) * x[1] * m[0]
+
+        mn = m[0] * m[1]    # combine mod classes
+
+        x = x[2:]           # remove first 2 remainders
+        x = [an % mn] + x   # prepend combined remainder
+
+        m = m[2:]           # remove first 2 mods
+        m = [mn] + m        # prepend combined mod
+    
+        if len(x) == 1:     # final remainder found
+            break
+    
+    return x[0], an, m      # returns final Xn, An, and Mn to satisy An = Xn mod(Mn)
     
 
 with open('input.txt') as f:
     lines = f.read().split('\n')
-    busses = {int(n):i for i, n in enumerate(lines[1].split(',')) if n != 'x'}
+    pairs = {int(n):i for i, n in enumerate(lines[1].split(',')) if n != 'x'}
 
-    time = lcm(busses.keys())
-    r_busses = list(busses.items()).reverse()
+    busses = list(pairs.keys())
+    offsets = list(pairs.values())
 
-    i = 1
-    while not is_proper_offset(time):
-        r = 
-        i += 1
-
-    print(busses, l)
+    r, an, mn = crt(busses, offsets)
+    print(mn[0] - r)    # subtract combined remainder from combined mod
